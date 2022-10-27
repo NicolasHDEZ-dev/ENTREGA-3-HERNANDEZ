68e13606a3510cf28e660320593de83b2191d5cd
Nics <nicsmusiccontact@gmail.com>
2022-10-27 18:56:59 -0300

    primer commit

---
+++ entrega-3-NicolasHernandez/stock.js
@@ -0,0 +1,14 @@
+let stockProductos = [
+    {id: 1, nombre: "Buzo", tipo: "buzo", cantidad: 1, desc: "Buzo Nike Air Rojo", precio: 12000, talle: "L", img: './img/buzorojo.jfif'},
+    {id: 2, nombre: "Buzo", tipo: "buzo", cantidad: 1, desc: "Buzo Nike Air Negro", precio: 11000, talle: "L", img: './img/buzonegro.jfif'},
+    {id: 3, nombre: "Buzo", tipo: "buzo", cantidad: 1, desc: "Buzo Nike NSW Blanco", precio: 12000, talle: "M", img: './img/buzoblanco.jfif'},
+    {id: 4, nombre: "Remera", tipo: "remera", cantidad: 1, desc: "Remera Nike Negra", precio: 5599, talle: "L", img: './img/remenike1.jfif'},
+    {id: 5, nombre: "Remera", tipo: "remera", cantidad: 1, desc: "Remera Nike Dri-fit", precio: 6700, talle: "L", img: './img/remenike2.jfif'},
+    {id: 6, nombre: "Remera", tipo: "remera", cantidad: 1, desc: "Remera Nike JDI", precio: 4500, talle: "M", img: './img/remenike3.jfif'},
+    {id: 7, nombre: "Short", tipo: "short", cantidad: 1, desc: "Short Nike Dri-fit Rojo", precio: 7000, talle: "L", img: './img/shortrojo.jfif'},
+    {id: 8, nombre: "Short", tipo: "short", cantidad: 1, desc: "Short Nike Dri-fit Celeste", precio: 6899, talle: "S", img: './img/shortceleste.jfif'},
+    {id: 9, nombre: "Short", tipo: "short", cantidad: 1, desc: "Short Nike Essentials", precio: 7000, talle: "L", img: './img/shortgris.jfif'},
+    {id: 10, nombre: "Pantalon", tipo: "pantalon", cantidad: 1, desc: "Pantalon Nike Negro", precio: 11000, talle: "L", img: './img/pantalonnegro.jfif'},
+    {id: 11, nombre: "Pantalon", tipo: "pantalon", cantidad: 1, desc: "Pantalon Nike Liverpool", precio: 12999, talle: "L", img: './img/liverpool.jfif'},
+    {id: 12, nombre: "Pantalon", tipo: "pantalon", cantidad: 1, desc: "Pantalon Nike Barcelona", precio: 12999, talle: "M", img: './img/barcelona.jfif'},
+]
